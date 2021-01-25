fd52b2a924a802ed90bf9e166bcb91e355f27c47
if-x <ifx.creator@gmail.com>
2021-01-25 14:48:29 +1100

    Remove redundant file

--- test.ts
+++
@@ -1,7 +0,0 @@
-import { getUnixTimeAfterMins, getUnixTimeNowInSec } from "./utils/timestamp";
-
-console.log(new Date().getTime());
-console.log(Date.now());
-
-console.log(getUnixTimeNowInSec());
-console.log(getUnixTimeAfterMins(10));
