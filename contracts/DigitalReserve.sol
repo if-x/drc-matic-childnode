40a7b4a8ff82588d9f83983690819c96f46604c5
if-x <ifx.creator@gmail.com>
2021-01-28 18:46:59 +1100

    Update import sequence

--- contracts/DigitalReserve.sol
+++ contracts/DigitalReserve.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.6.6;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 import "./interfaces/Uniswap/IUniswapV2Router02.sol";
 import "./interfaces/IDigitalReserve.sol";
